d2f0e18183d190dba6409991dc9f9a249a5ad07f
Eduardo Carrillo López <ed.carrillo.l97@hotmail.com>
2019-03-28 19:42:28 -0600

    Segunda vista terminada visualmente, inicio de código (p135)

---
+++ Project1/Project1/ImagesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ImagesViewController.swift
+//  Project1
+//
+//  Created by Eduardo Carrillo on 3/28/19.
+//  Copyright © 2019 Eduardo Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ImagesViewController: UIViewController {
+    
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet var creditLabel: UILabel!
+    //p135 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
