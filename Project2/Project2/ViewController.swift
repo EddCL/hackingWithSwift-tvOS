14eea3fd86c044385c0b3fd827c407313e894c3e
Eduardo Carrillo <ed.carrillo.l97@hotmail.com>
2019-05-09 14:13:01 -0500

    Terminada la interfaz de la aplicación, comienzo con el código

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Eduardo Carrillo on 5/8/19.
+//  Copyright © 2019 Eduardo Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var result: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
