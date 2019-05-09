ee5c3eca640159312ae21da0f29d7ff7d6467ae2
Eduardo Carrillo <ed.carrillo.l97@hotmail.com>
2019-05-09 14:49:45 -0500

    Continuación con proyecto 2, p172

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -8,15 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var result: UIImageView!
+    
+    var activeCells = [IndexPath]()
+    var flashSequence = [IndexPath]()
+    var levelCounter = 0
+    
+    let levels = [[6,7,8],
+                  [1,3,11,13],
+                  [5,6,7,8,9],
+                  [0,4,5,9,10,14],
+                  [1,2,3,7,11,12,13],
+                  [0,2,4,5,9,10,12,14],
+                  [1, 2, 3, 6, 7, 8, 11, 12, 13],
+                  [0, 1, 2, 3, 4, 10, 11, 12, 13, 14],
+                  [1, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13],
+                  [0, 1, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14],
+                  [0, 1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14],
+                  [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14],
+                  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+    }
+    
+    func createLevel(){
+        //pagina172
+    }
+    
 
 
 }
