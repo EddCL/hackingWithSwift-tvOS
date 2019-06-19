88b07dc8d39253176468394a877784446fafe6da
Eduardo Carrillo <ed.carrillo.l97@hotmail.com>
2019-06-19 11:26:06 -0500

    Continuación proyecto 3

--- Project3/Project3/ViewController.swift
+++ Project3/Project3/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textFieldTip: UILabel!
+    @IBOutlet var nextButton: UIButton!
+    
+    var focusGuide: UIFocusGuide!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        focusGuide = UIFocusGuide()
+        view.addLayoutGuide(focusGuide)
+        
+        focusGuide.topAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
+        focusGuide.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        focusGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
     }
 
-
+    @IBAction func showAlert(_ sender: UIButton) {
+    }
+    
 }
 
